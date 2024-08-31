214a74656ed506cbe7d9853bda3f87aac259e1f9
swetanshu <swetanshusingh17@gmail.com>
2024-08-31 22:21:10 +0530

    [update]: minor changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,13 @@ import Logo from "./ui/logo";
 import NavLinks from "./ui/nav-links";
 
 const Navbar = () => {
-    return (
-        <nav className="w-5/6 min-h-14 bg-white flex justify-between items-center rounded-2xl p-5">
-            <Logo />
-            <NavLinks />
-            <p>Sign up</p>
-        </nav>
-    )
-}
+  return (
+    <nav className="w-5/6 bg-white flex justify-between items-center rounded-2xl p-4">
+      <Logo />
+      <NavLinks />
+      <p className="text-2xl font-bold">Sign up</p>
+    </nav>
+  );
+};
 
 export default Navbar;
